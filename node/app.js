a700fd896ff5fd717710604bb80b075f1dfca14d
John Pettigrew <john.pettigrew404@gmail.com>
2015-09-12 01:25:19 -0500

    save more data with tweets

--- node/app.js
+++ node/app.js
@@ -2,6 +2,7 @@ var settings = require('./settings.json');
 
 var MongoClient = require('mongodb').MongoClient;
 var Twitter = require('node-tweet-stream');
+var moment = require('moment');
 var t = new Twitter(settings);
 
 MongoClient.connect('mongodb://mongo:27017/walking_dead', function(err, db){
@@ -10,6 +11,14 @@ MongoClient.connect('mongodb://mongo:27017/walking_dead', function(err, db){
     var tweetsCollection = db.collection('tweets');
 
     t.on('tweet', function(tweet){
+
+      //Add data to help queries later
+      var now = moment(tweet.timestamp_ms, 'x').utc();
+      tweet.now.dayOfYear = now.dayOfYear();
+      tweet.now.dayOfMonth = now.date();
+      tweet.now.dayOfWeek = now.weekday();
+      tweet.now.hour = now.hour();
+      tweet.now.minute = now.minute();
 
       tweetsCollection.insert(tweet, function(err){
         if(err){
